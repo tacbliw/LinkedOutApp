9671cc5f9af2b7a3543082afa28d002a16a49083
Nguyễn Phú Trường <khoahockithuatmc@gmail.com>
2020-12-08 17:20:18 +0700

    redesign follow, add service to follow

--- app/screens/following-screen/following-screen.tsx
+++ app/screens/following-screen/following-screen.tsx
@@ -1,201 +1,106 @@
-import { Badge, Icon, Thumbnail, View } from 'native-base'
-import { FlatList, TouchableOpacity } from 'react-native'
+import { Card, CardItem, Header, Icon, Left, Text, Thumbnail, View } from 'native-base'
+import { FlatList, ScrollView, TouchableOpacity, useWindowDimensions } from 'react-native'
 import React from 'reactn'
-import { Screen, Text } from '../../components'
-import { color } from '../../theme'
+import { Screen } from '../../components'
+import { toBackendUrl } from '../../helpers/string-helper'
+import { UserFollowedObject } from '../../repositories/follow-repository'
+import { followService } from '../../services/follow-service'
+import { color } from '../../theme/color'
 import { styles } from './styles'
-const pratik = require('./avatar.jpg')
-const sanket = require('./avatar.jpg')
-const megha = require('./avatar.jpg')
-const atul = require('./avatar.jpg')
-const saurabh = require('./avatar.jpg')
-const varun = require('./avatar.jpg')
-
-const datas = [
-  {
-    img: pratik,
-    title: 'Kumar Pratik',
-    note: 'Its time to build a difference . .',
-    time: 'a minutes ago',
-    type: 'company',
-  },
-  {
-    img: sanket,
-    title: 'Kumar Sanket',
-    note: 'One needs courage to be happy and smiling all time . . ',
-    time: '1:12 pm',
-    type: 'company',
-  },
-  {
-    img: megha,
-    title: 'Megha',
-    note: 'Live a life style that matchs your vision',
-    time: '10:03 am',
-    type: 'company',
-  },
-  {
-    img: atul,
-    title: 'Atul Ranjan',
-    note: 'Failure is temporary, giving up makes it permanent',
-    time: '5:47 am',
-    type: 'user',
-  },
-  {
-    img: saurabh,
-    title: 'Saurabh Sahu',
-    note: 'The biggest risk is a missed opportunity !!',
-    time: '11:11 pm',
-    type: 'user',
-  },
-  {
-    img: varun,
-    title: 'Varun Sahu',
-    note: '',
-    time: '8:54 pm',
-    type: 'user',
-  },
-  {
-    img: varun,
-    title: 'Varun Sahu',
-    note: '',
-    time: '8:54 pm',
-    type: 'user',
-  },
-  {
-    img: varun,
-    title: 'Varun Sahu',
-    note: '',
-    time: '8:54 pm',
-    type: 'user',
-  },
-  {
-    img: varun,
-    title: 'Varun Sahu',
-    note: '',
-    time: '8:54 pm',
-    type: 'user',
-  },
-  {
-    img: varun,
-    title: 'Varun Sahu',
-    note: '',
-    time: '8:54 pm',
-    type: 'user',
-  },
-]
-
-const map_icon_notification = {}
-map_icon_notification['company'] = {
-  name: 'heart',
-  backgroundColor: color.brandDanger,
-}
-map_icon_notification['user'] = {
-  name: 'thumbs-up',
-  backgroundColor: color.brandPrimary,
-}
-// map_icon_notification['notification'] = {
-//   name: 'notifications',
-//   backgroundColor: color.brandSuccess,
-// }
-
-const map_notification_type = {}
-map_notification_type['company'] = 'Company'
-map_notification_type['user'] = 'User '
-//map_notification_type['notification'] = 'inform you, '
-
-const Item = ({ item, onPress, style }) => (
-  <TouchableOpacity onPress={onPress} style={style}>
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      <View style={{ position: 'relative' }}>
-        <Thumbnail source={item.img}></Thumbnail>
-        <Badge
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            right: 0,
-            borderWidth: 2,
-            borderColor: color.background,
-            backgroundColor:
-              map_icon_notification[item.type]['backgroundColor'],
-          }}
-        >
-          <Icon
-            name={map_icon_notification[item.type]['name']}
-            style={{ fontSize: 16, color: '#ffffff' }}
-          ></Icon>
-        </Badge>
-      </View>
-      <View style={styles.info}>
-        <Text style={styles.name}>{item.title}</Text>
-        <Text style={styles.type}>{map_notification_type[item.type]}</Text>
-      </View>
-    </View>
-  </TouchableOpacity>
-)
 
 export const FollowingScreen = function FollowingScreen(navigation) {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
-  // OR
-  // const rootStore = useStores()
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  const following_width = useWindowDimensions().width/2 - 50;
+  const following_max_height = useWindowDimensions().height/3;
 
-  // const [loading, setLoading] = useState(false);
-  // const [dataSource, setDataSource] = useState([]);
-  // const [offset, setOffset] = useState(1);
-  // const [isListEnd, setIsListEnd] = useState(false);
-
-  // return (
-  // <Screen style={styles.container} preset="scroll">
-  //   {/* <Text preset="header" text="followingScreen" /> */}
-  //   {/* <SafeAreaView style={{flex: 1}}>
-  //   <FlatList
-  //     data={dataSource}
-  //     keyExtractor={(item, index) => index.toString()}
-  //     renderItem={ItemView}
-  //     ListFooterComponent={renderFooter}
-  //     onEndReached={getData}
-  //     onEndReachedThreshold={0.5}
-  //   />
-  // </SafeAreaView>
-  //  */}
-  //  <Text style={styles.header}>FOLLOWING</Text>
-  //  {/* <SafeAreaView style={{flex:1,}}>
-
-  //    <FlatList>
-
-  //    </FlatList>
-
-  //  </SafeAreaView> */}
-
-  // </Screen>
-
-  const renderItem = ({ item }) => {
+  const EmptyCard = () => {
     return (
-      <Item
-        item={item}
-        onPress={() => {}} //
-        style={styles.line}
-      ></Item>
+      // <Card transparent style={{width: following_width, minHeight: 400, marginLeft: 15, marginRight: 15}}>
+      // <CardItem style={styles.cardBody}>
+        <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+          <Icon name="body"></Icon>
+          <Text> Wow, such empty </Text>
+          <Icon name="body"></Icon>
+        </View>
+      // </CardItem>
+    // </Card>
     )
   }
 
+  const renderItem = ({ item } : {item: UserFollowedObject}) => {
+    console.log(toBackendUrl(item.profilePicture));
+    return (
+      <Card transparent style={{width: following_width, maxHeight: following_max_height, marginLeft: 15, marginRight: 15}}>
+        <CardItem header style={styles.cardHeader}>
+          {/* <Thumbnail ></Thumbnail> */}
+          {/* <FastImage source={{uri: toBackendUrl(item.profilePicture)}} style={{width: 60}}></FastImage> */}
+          <Thumbnail source={{ uri: toBackendUrl(item.profilePicture) }}></Thumbnail>
+        </CardItem>
+        <CardItem style={styles.cardBody}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>{item.firstname + " " + item.lastname}</Text>
+            {/* <Text>{item.description}</Text> */}
+          </View>
+        </CardItem>
+
+        <CardItem style={styles.cardFooter} footer>
+          <View style={{flexGrow: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+            <Text>{item.followedCount} followers</Text>
+            <TouchableOpacity onPress={() => { 
+              deleteFollow(item.id);
+              }}>
+              <Icon style={{color: color['color-danger-500'] }} name="leaf"></Icon>
+            </TouchableOpacity>
+          </View>
+        </CardItem>
+      </Card>
+    )
+  }
+
+  const [
+    users, 
+    refreshing, 
+    handleLoadNew,
+    handleItemPress,
+    deleteFollow
+  ] = followService.useUserFollowed()
+
+  // const [users, setUsers] = React.useState<UserFollowedResponse>([])
+
   return (
     <Screen style={styles.container}>
+      <ScrollView>
       {/* <Header transparent> */}
       {/* <Left style={{ flexGrow: 1 }}>
         <Text style={{ fontSize: 32, fontWeight: '700' }}>Notifications</Text>
       </Left>
       <Right></Right> */}
-      <Text style={styles.header}>FOLLOWING</Text>
+      <Header transparent>
+        <Left style={{ flexGrow: 1 }}>
+          <Text style={styles.header}>Following</Text>
+        </Left>
+      </Header>
+
       {/* </Header> */}
       <FlatList
-        data={datas}
+        contentContainerStyle={{
+          flexDirection: 'column',
+          // flexWrap: 'wrap',
+          // justifyContent: 'center',
+          // alignItems: '',
+          // alignContent: 'center',
+          // alignItems: 'stretch',
+          marginTop: 36,
+          marginBottom: 36,
+          marginLeft: 16
+        }}
+        numColumns={2}
+        data={users}
         renderItem={renderItem}
-        keyExtractor={(item) => item.title}
+        keyExtractor={(item) => item.id.toString()}
+        ListEmptyComponent={() => EmptyCard()}
       ></FlatList>
+      </ScrollView>
     </Screen>
   )
 }
--- app/screens/following-screen/styles.tsx
+++ app/screens/following-screen/styles.tsx
@@ -1,68 +1,41 @@
 import { Dimensions, StyleSheet } from 'react-native'
-import { color } from '../../theme'
+import { color } from '../../theme/color'
+
 
 const { width, height } = Dimensions.get('screen')
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: color.backgroundSetting,
   },
-  //   header:{
-  //     backgroundColor:color.brandDanger,
-  //     color: color.palette.white,
-  //     fontFamily: "roboto-regular",
-  //     fontSize:50,
-  //     width: width,
-  //     height: height/6,
-  //     justifyContent:'center',
-  //     textAlign:'center',
-  //     elevation: 20,
-  //     borderRadius: 2,
-  //   },
   header: {
-    backgroundColor: color.brandDanger,
-    color: color.palette.white,
-    fontSize: 40,
-    width: width,
-    height: height / 8,
-    justifyContent: 'center',
-    textAlign: 'center',
-    //marginLeft: width*0.05,
-    elevation: 20,
-    borderRadius: 2,
-    fontWeight: '700',
-  },
-  line: {
-    marginVertical: height * 0.01,
-    backgroundColor: '#f9fbe7',
-    //elevation: 50,
-    marginLeft: 0,
-    height: height * 0.1,
-    width: width,
-    justifyContent: 'center',
-  },
-  text: {
-    color: color.brandDark,
-    fontSize: 25,
-    alignSelf: 'center',
-    width: width * 0.65,
-    fontWeight: '700',
-    //  backgroundColor:color.brandPrimary
-  },
-  info: {
-    flex: 1,
-    marginLeft: width * 0.05,
-    flexDirection: 'column',
-    alignItems: 'stretch',
-  },
-  name: {
-    color: color.brandDark,
-    fontSize: 20,
+    fontSize: 36,
     fontWeight: '700',
   },
 
-  type: {
-    color: color.pickerOption,
-    fontSize: 15,
+  cardHeader: {
+    borderColor: color['color-gray-200'], 
+    borderTopWidth: 1, 
+    borderLeftWidth: 1, 
+    borderRightWidth: 1, 
+    borderTopLeftRadius: 10, 
+    borderTopRightRadius: 10,
+    backgroundColor: color['color-gray-200']
   },
+
+  cardBody: {
+    borderColor: color['color-gray-200'], 
+    borderLeftWidth: 1, 
+    borderRightWidth: 1,
+    backgroundColor: color['color-gray-200']
+  },
+
+  cardFooter: {
+    borderColor: color['color-gray-200'], 
+    borderBottomWidth: 1, 
+    borderLeftWidth: 1, 
+    borderRightWidth: 1,
+    borderBottomLeftRadius: 10, 
+    borderBottomRightRadius: 10,
+    backgroundColor: color['color-gray-200']
+  }
 })
